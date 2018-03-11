a9f29a5e969ca7a1e40b02b6332f1ff6b04ce908
Gabriel S. Álvarez <galvar3@bancodebogota.com.co>
2018-03-11 16:57:10 -0500

    Initial commit for backend

---
+++ TICS/Implementación/hsp-mngr/src/main/java/com/poli/hspmngr/HspMngrApplication.java
@@ -0,0 +1,12 @@
+package com.poli.hspmngr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HspMngrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HspMngrApplication.class, args);
+	}
+}
