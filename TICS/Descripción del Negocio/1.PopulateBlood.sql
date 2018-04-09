f8db92d27b380717fdfb76400e0c2d2894251368
Gabriel S. Álvarez <gabo05640@hotmail.com>
2018-04-08 21:33:53 -0500

    Script for populate blood table

---
+++ TICS/Descripción del Negocio/1.PopulateBlood.sql
@@ -0,0 +1,8 @@
+INSERT INTO hsp.blood (type, volume) VALUES ('A+', 0);
+INSERT INTO hsp.blood (type, volume) VALUES ('A-', 0);
+INSERT INTO hsp.blood (type, volume) VALUES ('B+', 0);
+INSERT INTO hsp.blood (type, volume) VALUES ('B-', 0);
+INSERT INTO hsp.blood (type, volume) VALUES ('O+', 0);
+INSERT INTO hsp.blood (type, volume) VALUES ('O-', 0);
+INSERT INTO hsp.blood (type, volume) VALUES ('AB+', 0);
+INSERT INTO hsp.blood (type, volume) VALUES ('AB-', 0);
