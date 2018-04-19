ffec4022ecd41f9cbb9fdc71c2ab7eead937569e
Gabriel S. Álvarez <gabo05640@hotmail.com>
2018-04-18 21:33:09 -0500

    Fix sql

--- TICS/Descripción del Negocio/1.PopulateBlood.sql
+++ TICS/Descripción del Negocio/1.PopulateBlood.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 BEGIN;
 
 INSERT INTO hsp.blood (type, volume) VALUES ('A+', 0);
@@ -11,13 +10,3 @@ INSERT INTO hsp.blood (type, volume) VALUES ('AB+', 0);
 INSERT INTO hsp.blood (type, volume) VALUES ('AB-', 0);
 
 COMMIT;
-=======
-INSERT INTO hsp.blood (type, volume) VALUES ('A+', 0);
-INSERT INTO hsp.blood (type, volume) VALUES ('A-', 0);
-INSERT INTO hsp.blood (type, volume) VALUES ('B+', 0);
-INSERT INTO hsp.blood (type, volume) VALUES ('B-', 0);
-INSERT INTO hsp.blood (type, volume) VALUES ('O+', 0);
-INSERT INTO hsp.blood (type, volume) VALUES ('O-', 0);
-INSERT INTO hsp.blood (type, volume) VALUES ('AB+', 0);
-INSERT INTO hsp.blood (type, volume) VALUES ('AB-', 0);
->>>>>>> 2f4f99f296f0138b2944998d303d8c4cbeadc0c5
